a94752a896814ea518465885e6ccf875b54618e5
Ritik <you@example.com>
2023-04-16 15:14:13 +0530

    NGINX doc page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./Pages/Landing";
 import CategoryOptimizations from "./Pages/CategoryOptimizations";
 import Demo from "./Pages/Demo";
 import K8Workloads from "./Pages/K8Workloads";
+import NginxDoc from "./Pages/NginxDoc";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       />
       <Route path="/:category/demo/:workload" element={<Demo />} />
       <Route path="/k8-workloads" element={<K8Workloads />} />
+      <Route path="/k8-workloads/rabmq-doc" element={<NginxDoc />} />
     </Routes>
   );
 };
