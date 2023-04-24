675bf17afec091527ca2843e30a0338519473aa1
Ritik <you@example.com>
2023-04-24 09:27:51 +0530

    Updated doc pages with links to github readme

--- src/Pages/NginxDoc.js
+++ src/Pages/NginxDoc.js
@@ -2,6 +2,7 @@ import React from "react";
 import Accordion from "../Components/Accordion";
 import Header from "../Components/Header";
 import CodeRenderer from "../Components/CodeRenderer";
+import DocLink from "../Components/DocLink";
 
 const NginxDoc = () => {
   return (
@@ -18,154 +19,16 @@ const NginxDoc = () => {
           available K8 cluster.
         </p>
         <Accordion heading={"Install HA Proxy on Server"}>
-          <p className="text-lg">
-            SSH into the server reserved for HA Proxy. First you need to install
-            haproxy on the server.
-          </p>
-          <CodeRenderer
-            language="bash"
-            value={`# Ubuntu
-sudo apt install haproxy -y
-
-# Cent OS
- yum install haproxy`}
-          />
-          <p className="text-lg">
-            Create a file <b>haproxy.cfg</b> at /etc/haproxy/haproxy.cfg and put
-            the following in it:
-          </p>
-          <CodeRenderer
-            value={`global
-          log /dev/log  local0 warning
-          chroot      /var/lib/haproxy
-          pidfile     /var/run/haproxy.pid
-          maxconn     4000
-          user        haproxy
-          group       haproxy
-          daemon
-      
-         stats socket /var/lib/haproxy/stats
-      
-      defaults
-        log global
-        option  httplog
-        option  dontlognull
-              timeout connect 5000
-              timeout client 50000
-              timeout server 50000
-      
-      
-      frontend k8_api
-          bind *:6443
-          option tcplog
-          mode tcp
-          default_backend k8s_api_server
-      
-      backend k8s_api_server
-          mode tcp
-          option tcp-check
-          balance roundrobin
-          option forwardfor
-          http-send-name-header Host
-          default-server inter 10s downinter 5s rise 2 fall 2 slowstart 60s maxconn 250 maxqueue 256 weight 100
-          server server1 <master-ip1>:6443 check
-          server server2 <master-ip2>:6443 check
-          server server3 <master-ip3>:6443 check`}
-          />
-          <p>Enable HA Proxy</p>
-          <CodeRenderer
-            value={`systemctl start haproxy
-systemctl enable haproxy`}
-          />
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/ha-nginx-ingress#install-ha-proxy-on-server" />
         </Accordion>
         <Accordion heading={"Install NGINX Ingress Controller"}>
-          <p className="text-lg">Add the NGINX Ingress helm repo</p>
-          <CodeRenderer
-            value={`helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx`}
-          />
-          <p className="Install the Controller">Install the Controller</p>
-          <CodeRenderer
-            value={`helm install nginx-ingress ingress-nginx/ingress-nginx --create-namespace --namespace nginx-ingress -f custom-values.yaml`}
-          />
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/ha-nginx-ingress#install-ha-proxy-on-server" />
         </Accordion>
         <Accordion heading="Configure">
-          <p className="text-lg">
-            The NGINX controller can be configured using the{" "}
-            <b>custom values.yaml</b> file. The contents we use are:
-          </p>
-          <CodeRenderer
-            language="yaml"
-            value={`
-## To have short names
-fullNameOverride: nginx-controller
-nameOverride: nginx-ingress
-
-controller:
-  name: controller
-  containerName: controller-container
-
-  watchIngressWithoutClass: false
-
-  ingressClassResource:
-    name: nginx-ingress
-    enabled: true
-    default: true
-    controllerValue: "k8s.io/ingress-nginx"
-
-  ingressClass: nginx-ingress
-  service:
-    type: NodePort
-    nodePorts:
-      http: 32080
-      https: 32443
-      tcp:
-        8080: 32808
-          `}
-          />
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/ha-nginx-ingress#create-the-custom-values-file" />
         </Accordion>
-        <Accordion heading="Security with TLS">
-          <p className="text-lg">
-            Security can be configured using the <b>spec.tls</b> key of ingress.
-            An example of secured Ingress:
-          </p>
-          <CodeRenderer
-            language="yaml"
-            value={`
-          apiVersion: networking.k8s.io/v1
-          kind: Ingress
-          metadata:
-            name: prometheus-ingress
-            namespace: monitoring
-          spec:
-            # tls:
-            #   - hosts:
-            #       - prometheus.dashboard
-            #       - grafana.dashboard
-            #     secretName: tls-secret
-            ingressClassName: nginx-ingress
-            rules:
-              - host: prometheus.dashboard
-                http:
-                  paths:
-                    - path: /
-                      pathType: Prefix
-                      backend:
-                        service:
-                          name: prom-prometheus
-                          port:
-                            number: 9090
-              - host: grafana.dashboard
-                http:
-                  paths:
-                    - path: /
-                      pathType: Prefix
-                      backend:
-                        service:
-                          name: monitoring-op-grafana
-                          port:
-                            number: 80
-          `}
-          />
+        <Accordion heading="Accessing the service through HA Proxy">
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/ha-nginx-ingress#accessing-the-service-through-ha-proxy" />
         </Accordion>
       </div>
     </>
--- src/Pages/PostgresDoc.js
+++ src/Pages/PostgresDoc.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Components/Header";
 import Accordion from "../Components/Accordion";
 import CodeRenderer from "../Components/CodeRenderer";
+import DocLink from "../Components/DocLink";
 
 const PostgresDoc = () => {
   return (
@@ -16,101 +17,26 @@ const PostgresDoc = () => {
           recovery built into it.
         </p>
         <Accordion heading={"Installing the Operator"}>
-          <p className="text-lg">Clone their GitHub repo:</p>
-          <CodeRenderer
-            value={`git clone https://github.com/CrunchyData/postgres-operator-examples pgo
-cd pgo`}
-          />
-          <p className="text-lg">Make a custom-values.yaml file:</p>
-          <CodeRenderer
-            language="yaml"
-            value={`
-resources:
-  controller:
-    limits:
-      memory: 12Gi
-      cpu: 8000m
-    requests:
-      memory: 8Gi
-      cpu: 4000m
-  upgrade: {}`}
-          />
-          <p className="text-lg">Install the operator using helm:</p>
-          <CodeRenderer
-            value={`helm install pgo --create-namespace --namespace postgres helm/install`}
-          />
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/pgsql#installing-the-operator" />
         </Accordion>
         <Accordion heading={"Configuring a Cluster"}>
-          <p className="text-lg">
-            A PostgresCluster using the operator looks like this:
-          </p>
-          <CodeRenderer
-            language="yaml"
-            value={`
-apiVersion: postgres-operator.crunchydata.com/v1beta1
-kind: PostgresCluster
-metadata:
-  name: hippo
-spec:
-  image: registry.developers.crunchydata.com/crunchydata/crunchy-postgres:ubi8-14.7-0
-  postgresVersion: 14
-  instances:
-    - name: instance1
-      dataVolumeClaimSpec:
-        accessModes:
-        - "ReadWriteOnce"
-        resources:
-          requests:
-            storage: 1Gi
-  backups:
-    pgbackrest:
-      image: registry.developers.crunchydata.com/crunchydata/crunchy-pgbackrest:ubi8-2.41-4
-      repos:
-      - name: repo1
-        volume:
-          volumeClaimSpec:
-            accessModes:
-            - "ReadWriteOnce"
-            resources:
-              requests:
-                storage: 1Gi
-          `}
-          />
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/pgsql#running-a-basic-postgres-cluster" />
         </Accordion>
         <Accordion heading={"Connect to a cluster"}>
-          <p className="text-lg">
-            The connection information like user, password, host and port is
-            stored in a secret which has the format of{" "}
-            <b>{"<cluster-name>-pguser-<username>"}</b>. The default user that
-            is created is same as the cluster name, in our case hippo. So the
-            secret is hippo-pguser-hippo. You can access the secret with:
-          </p>
-          <CodeRenderer
-            value={" kubectl describe -n postgres secret hippo-pguser-hippo"}
-          />
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/pgsql#connecting-to-our-cluster" />
         </Accordion>
         <Accordion heading={"Setup HA Cluster"}>
           PGO protects our cluster from downtime in case anything goes wrong by
-          having multiple instances of a postgres instance. There are two ways
-          an HA cluster can be set up:
-          <ul>
-            <li>Increase the spec.instances.replicas value</li>
-            <li>Adding another instance to spec.instances</li>
-          </ul>
-          <p>
-            This will setup multiple postgres pods that will elect 1 master
-            amongst themselves and if the master goes down, a pod will be
-            assigned master immediately and PGO will bring up the other pod.
-          </p>
+          having multiple instances of a postgres instance.
+          <div className="mt-2" />
+          <DocLink link="https://github.com/akarX23/k8-deployments/tree/master/pgsql#setting-up-a-high-availability-cluster" />
         </Accordion>
         <Accordion heading={"TLS/SSL Configuration"}>
-          <p className="text-lg">
-            By default PGO will use TLS for all internal communication. It
-            generates its own TLS keys and certificates which it stores as
-            secrets. We can use our own certificates and keys as well using the
-            spec.customTLSSecret. We need to make a secret first where we put
-            our certificates and key.
-          </p>
+          <DocLink
+            link={
+              "https://github.com/akarX23/k8-deployments/tree/master/pgsql#tlsssl"
+            }
+          />
         </Accordion>
       </div>
     </>
